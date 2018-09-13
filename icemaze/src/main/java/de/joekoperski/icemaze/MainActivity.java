ddf256f20e42ae7a1268bccbc3078a2bfa64a665
GammaSoldier <spam.joekoperski@gmx.de>
2018-09-13 21:10:11 +0200

    - GameView as surfaceView

--- icemaze/src/main/java/de/joekoperski/icemaze/MainActivity.java
+++ icemaze/src/main/java/de/joekoperski/icemaze/MainActivity.java
@@ -6,21 +6,26 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     Rules theRules;
-    gridBitmap theGridBitmap;
-
+//    gridBitmap theGridBitmap;
+    GameView theGridBitmap;
+    LinearLayout surface;
     ////////////////////////////////////////////////////////////////////////////////////////////////
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        theGridBitmap = new gridBitmap(this);
+//        theGridBitmap = new gridBitmap(this);
+        theGridBitmap = new GameView(this);
 
+        surface = (LinearLayout)findViewById(R.id.imageView);
+        surface.addView(theGridBitmap);
 
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +37,13 @@ public class MainActivity extends Activity {
         });
 
         View contentView = (View) findViewById(R.id.mainLayout);
+        contentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
         contentView.setOnTouchListener(new OnSwipeTouchListener(this) {
             public void onSwipeTop() {
                 long startTime = System.currentTimeMillis();
