2e4975e7d99da0ad4dbe15cf42754100dffe9688
GammaSoldier <36199916+GammaSoldier@users.noreply.github.com>
2018-09-24 14:26:05 +0200

    - load and save working (dirty code)

--- icemaze/src/main/java/de/joekoperski/icemaze/EditorActivity.java
+++ icemaze/src/main/java/de/joekoperski/icemaze/EditorActivity.java
@@ -2,17 +2,15 @@ package de.joekoperski.icemaze;
 
 import android.Manifest;
 import android.app.Activity;
-import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Point;
 import android.net.Uri;
-import android.os.Environment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -23,20 +21,19 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.prefs.Preferences;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class EditorActivity extends Activity {
     public static final int REQUEST_WRITE_STORAGE = 112;
     public static final int FILE_SELECT_CODE = 123;
 
     private GameViewEditor theGridBitmap;
-    private RelativeLayout surface;
     private int selectedTile;
     private int width;
     private int height;
@@ -55,13 +52,13 @@ public class EditorActivity extends Activity {
 
         theGridBitmap = new GameViewEditor(this);
 
-        surface = (RelativeLayout) findViewById(R.id.gridView);
+        RelativeLayout surface = findViewById(R.id.gridView);
         surface.addView(theGridBitmap);
 
         selectedTile = TileID.TILE_ICE;
 
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonTest = (Button) findViewById(R.id.buttonTest);
+        Button buttonTest = findViewById(R.id.buttonTest);
         buttonTest.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent myIntent = new Intent(getBaseContext(), MainActivity.class);
@@ -73,7 +70,7 @@ public class EditorActivity extends Activity {
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonLoad = (Button) findViewById(R.id.buttonLoad);
+        Button buttonLoad = findViewById(R.id.buttonLoad);
         buttonLoad.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 loadFile();
@@ -82,7 +79,7 @@ public class EditorActivity extends Activity {
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonSave = (Button) findViewById(R.id.buttonSave);
+        Button buttonSave = findViewById(R.id.buttonSave);
         buttonSave.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 saveFile();
@@ -92,7 +89,7 @@ public class EditorActivity extends Activity {
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonIce = (Button) findViewById(R.id.buttonIce);
+        Button buttonIce = findViewById(R.id.buttonIce);
         buttonIce.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selectedTile = TileID.TILE_ICE;
@@ -101,7 +98,7 @@ public class EditorActivity extends Activity {
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonRock = (Button) findViewById(R.id.buttonRock);
+        Button buttonRock = findViewById(R.id.buttonRock);
         buttonRock.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selectedTile = TileID.TILE_ROCK;
@@ -110,7 +107,7 @@ public class EditorActivity extends Activity {
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonGrowRock = (Button) findViewById(R.id.buttonGrowRock);
+        Button buttonGrowRock = findViewById(R.id.buttonGrowRock);
         buttonGrowRock.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selectedTile = TileID.TILE_GROW_ROCK;
@@ -119,7 +116,7 @@ public class EditorActivity extends Activity {
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonLock = (Button) findViewById(R.id.buttonLock);
+        Button buttonLock = findViewById(R.id.buttonLock);
         buttonLock.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selectedTile = TileID.TILE_LOCK;
@@ -128,7 +125,7 @@ public class EditorActivity extends Activity {
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonKey = (Button) findViewById(R.id.buttonKey);
+        Button buttonKey = findViewById(R.id.buttonKey);
         buttonKey.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selectedTile = TileID.TILE_KEY;
@@ -137,7 +134,7 @@ public class EditorActivity extends Activity {
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonRedirNorth = (Button) findViewById(R.id.buttonRedirNorth);
+        Button buttonRedirNorth = findViewById(R.id.buttonRedirNorth);
         buttonRedirNorth.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selectedTile = TileID.TILE_REDIRECT_NORTH;
@@ -146,7 +143,7 @@ public class EditorActivity extends Activity {
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonRedirSouth = (Button) findViewById(R.id.buttonRedirSouth);
+        Button buttonRedirSouth = findViewById(R.id.buttonRedirSouth);
         buttonRedirSouth.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selectedTile = TileID.TILE_REDIRECT_SOUTH;
@@ -155,7 +152,7 @@ public class EditorActivity extends Activity {
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonRedirWest = (Button) findViewById(R.id.buttonRedirWest);
+        Button buttonRedirWest = findViewById(R.id.buttonRedirWest);
         buttonRedirWest.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selectedTile = TileID.TILE_REDIRECT_WEST;
@@ -164,7 +161,7 @@ public class EditorActivity extends Activity {
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonRedirEast = (Button) findViewById(R.id.buttonRedirEast);
+        Button buttonRedirEast = findViewById(R.id.buttonRedirEast);
         buttonRedirEast.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selectedTile = TileID.TILE_REDIRECT_EAST;
@@ -173,7 +170,7 @@ public class EditorActivity extends Activity {
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonTeleport = (Button) findViewById(R.id.buttonTeleport);
+        Button buttonTeleport = findViewById(R.id.buttonTeleport);
         buttonTeleport.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selectedTile = TileID.TILE_TELEPORT;
@@ -182,7 +179,7 @@ public class EditorActivity extends Activity {
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonTeleportTarget = (Button) findViewById(R.id.buttonTeleportTarget);
+        Button buttonTeleportTarget = findViewById(R.id.buttonTeleportTarget);
         buttonTeleportTarget.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selectedTile = TileID.TILE_TELEPORT_TARGET;
@@ -191,7 +188,7 @@ public class EditorActivity extends Activity {
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonStickOnce = (Button) findViewById(R.id.buttonStickOnce);
+        Button buttonStickOnce = findViewById(R.id.buttonStickOnce);
         buttonStickOnce.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selectedTile = TileID.TILE_STICK_ONCE;
@@ -200,7 +197,7 @@ public class EditorActivity extends Activity {
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonOnewayNorth = (Button) findViewById(R.id.buttonOnewayNorth);
+        Button buttonOnewayNorth = findViewById(R.id.buttonOnewayNorth);
         buttonOnewayNorth.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selectedTile = TileID.TILE_ONEWAY_NORTH;
@@ -209,7 +206,7 @@ public class EditorActivity extends Activity {
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonOnewaySouth = (Button) findViewById(R.id.buttonOnewaySouth);
+        Button buttonOnewaySouth = findViewById(R.id.buttonOnewaySouth);
         buttonOnewaySouth.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selectedTile = TileID.TILE_ONEWAY_SOUTH;
@@ -218,7 +215,7 @@ public class EditorActivity extends Activity {
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonOnewayEast = (Button) findViewById(R.id.buttonOnewayEast);
+        Button buttonOnewayEast = findViewById(R.id.buttonOnewayEast);
         buttonOnewayEast.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selectedTile = TileID.TILE_ONEWAY_EAST;
@@ -227,7 +224,7 @@ public class EditorActivity extends Activity {
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonOnewayWest = (Button) findViewById(R.id.buttonOnewayWest);
+        Button buttonOnewayWest = findViewById(R.id.buttonOnewayWest);
         buttonOnewayWest.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selectedTile = TileID.TILE_ONEWAY_WEST;
@@ -236,7 +233,7 @@ public class EditorActivity extends Activity {
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonKill = (Button) findViewById(R.id.buttonKill);
+        Button buttonKill = findViewById(R.id.buttonKill);
         buttonKill.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selectedTile = TileID.TILE_KILL;
@@ -245,7 +242,7 @@ public class EditorActivity extends Activity {
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonFinish = (Button) findViewById(R.id.buttonFinish);
+        Button buttonFinish = findViewById(R.id.buttonFinish);
         buttonFinish.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selectedTile = TileID.TILE_FINISH;
@@ -254,7 +251,7 @@ public class EditorActivity extends Activity {
 
 
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonStart = (Button) findViewById(R.id.buttonStart);
+        Button buttonStart = findViewById(R.id.buttonStart);
         buttonStart.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 selectedTile = TileID.TILE_START;
@@ -264,13 +261,13 @@ public class EditorActivity extends Activity {
 
         ////////////////////////////////////////////////////////////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////////////////////////
-        Button buttonGenerate = (Button) findViewById(R.id.buttonGenerate);
+        Button buttonGenerate = findViewById(R.id.buttonGenerate);
         buttonGenerate.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // read Editviews
-                EditText edit = (EditText) findViewById(R.id.editTextWidth);
+                EditText edit = findViewById(R.id.editTextWidth);
                 width = Integer.parseInt(edit.getText().toString());
-                edit = (EditText) findViewById(R.id.editTextHeight);
+                edit = findViewById(R.id.editTextHeight);
                 height = Integer.parseInt(edit.getText().toString());
                 generateMap(width, height);
             }// onClick
@@ -325,23 +322,43 @@ public class EditorActivity extends Activity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == FILE_SELECT_CODE && resultCode == RESULT_OK && data != null) {
+            File root = android.os.Environment.getExternalStorageDirectory();
+            File dir = new File(root.getAbsolutePath() + "/IceMaze");
+
             Uri uploadfileuri = data.getData();
             File file = new File(uploadfileuri.getPath());
-//            file = new File(uploadfileuri.getRawPath());
-            Toast.makeText(EditorActivity.this, "File load: " + file, Toast.LENGTH_LONG).show();
-            // TODO: 23.09.2018: load file content and build level map
-            if (file != null) {
+
+            final String[] split = file.getPath().split("/");
+            String filePath = dir + "/" + split[split.length - 1];
+
+            if (filePath != null) {
                 try {
-                    FileInputStream f = new FileInputStream(file);
+                    FileInputStream f = new FileInputStream(filePath);
                     DataInputStream din = new DataInputStream(f);
 
-                    int i = din.readInt();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
+                    int width = din.readInt();
+                    int height = din.readInt();
+                    theMap = new Map(width, height);
+                    for (int i = 0; i < theMap.getWidth(); i++) {
+                        for (int j = 0; j < theMap.getHeight(); j++) {
+                            theMap.setSourceMap(i, j, din.readInt());
+                            if (theMap.getSourceMap(i, j) == TileID.TILE_START) {
+                                thePlayer = new PlayerCharacter(new Point(i, j));
+                            }// if
+                        }// for j
+                    }// for i
+                    theGridBitmap.setDimensions(width, height);
+                    theGridBitmap.render(this, theMap, thePlayer, true);
+                    f.close();
+                    Toast.makeText(this, "File loaded", Toast.LENGTH_SHORT).show();
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-            }
+            }// if
+            else {
+                Toast.makeText(this, "File NOT loaded: " + filePath, Toast.LENGTH_SHORT).show();
+            }// else
         }// if
     }// onActivityResult
 
@@ -357,27 +374,32 @@ public class EditorActivity extends Activity {
         else {
             // You are allowed to write external storage:
             File root = android.os.Environment.getExternalStorageDirectory();
-            File dir = new File(root.getAbsolutePath() + "/Download");
-            dir.mkdirs();
-            File file = new File(dir, "icemaze.txt");
+            File dir = new File(root.getAbsolutePath() + "/IceMaze");
+            if (!dir.mkdirs()) {
+                Log.d("EditorActivity", "could not create directory: " + dir);
+            }
+
+            SimpleDateFormat simpleDate = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+            String strDt = simpleDate.format(new Date());
+
+            File file = new File(dir, "icemaze" + strDt + ".bin");
             Log.d("EditorActivity", "write file to: " + file);
             try {
                 FileOutputStream f = new FileOutputStream(file);
-                PrintWriter pw = new PrintWriter(f);
-                pw.print(theMap.getWidth());
-                pw.print(theMap.getHeight());
+                DataOutputStream dout = new DataOutputStream(f);
+
+                dout.writeInt(theMap.getWidth());
+                dout.writeInt(theMap.getHeight());
+
                 for (int i = 0; i < theMap.getWidth(); i++) {
                     for (int j = 0; j < theMap.getHeight(); j++) {
-                        pw.print(theMap.getSourceMap(i, j));
-                    }
-                }
-//                pw.flush();
-                pw.close();
+                        dout.writeInt(theMap.getSourceMap(i, j));
+                    }// for j
+                }// for i
+
                 f.close();
             }// try
-            catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }// catch
+            // catch
             catch (IOException e) {
                 e.printStackTrace();
             }// catch
@@ -387,7 +409,7 @@ public class EditorActivity extends Activity {
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
         switch (requestCode) {
