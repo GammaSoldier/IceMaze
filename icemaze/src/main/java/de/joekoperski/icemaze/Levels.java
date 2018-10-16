7dedb90abf1d52878de2b65e8947b8ec46b9707f
GammaSoldier <contact@joekoperski.de>
2018-10-17 00:28:03 +0200

    - prepare class Levels

---
+++ icemaze/src/main/java/de/joekoperski/icemaze/Levels.java
@@ -0,0 +1,18 @@
+package de.joekoperski.icemaze;
+
+import java.util.ArrayList;
+import java.util.logging.Level;
+
+public class Levels {
+
+    private static ArrayList<Level> levelArray;
+    private int map[][];
+
+    public Levels() {
+        levelArray = new ArrayList<Level>();
+
+
+
+        levelArray.add(new Level());
+    }// Levels
+}
